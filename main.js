b60a8985bbc881252953a6f0e12086b87da5b0c5
Evan Hopper-Moore <evanhoppermoore@gmail.com>
2017-04-05 05:45:07 +0000

    Cleanup and comments

--- main.js
+++ main.js
@@ -1,31 +1,31 @@
 $(document).ready(function() {
-    // $("#main").fadeOut(0);
+    // $("#content").fadeOut(0);
     // $("#title").slideUp(0);
-    
+
     $("#aboutusdropdown").slideUp(0);
     $("#practicesdropdown").slideUp(0);
     $("#blogdropdown").slideUp(0);
     $("#contactdropdown").slideUp(0);
-    
+
 
     $("#aboutuswrapper").hover(function() {
         $("#aboutusdropdown").stop().slideDown('slow');
     }, function() {
         $("#aboutusdropdown").stop().slideUp('slow');
     });
-    
+
     $("#practiceswrapper").hover(function() {
         $("#practicesdropdown").stop().slideDown('slow');
     }, function() {
         $("#practicesdropdown").stop().slideUp('slow');
     });
-    
+
     $("#blogwrapper").hover(function() {
         $("#blogdropdown").stop().slideDown('slow');
     }, function() {
         $("#blogdropdown").stop().slideUp('slow');
     });
-    
+
     $("#contactwrapper").hover(function() {
         $("#contactdropdown").stop().slideDown('slow');
     }, function() {
