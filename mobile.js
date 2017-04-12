c6fcd4ac2ad69d737a59b5a5a9ae409554767afa
verycumbersome <matthewsages@gmail.com>
2017-04-11 17:37:27 -0700

    Fixed mobile js

--- mobile.js
+++ mobile.js
@@ -8,24 +8,23 @@ $(document).ready(function() {
     $("#content h2").fadeOut(0);
 
     $("#logo").fadeIn(1000);
-    $("#menu").fadeIn(1000, function() {
+    $("#menu").fadeIn(1000);
+
+    // $("#content p").fadeIn(1750);
+
+    $("#title").animate({
+        marginLeft: '0%'
+    }, 1750, 'swing', function() {
+        // $("#content h2").fadeIn(1000);
+        // $("#content p").fadeIn(1000);
+    });
+
+    $("#subtitle").animate({
+        marginLeft: '0%'
+    }, 2250, 'swing', function() {
         $("#content h2").fadeIn(1000);
         $("#content p").fadeIn(1000);
     });
-    // $("#content p").fadeIn(1750);
-
-    // $("#title").animate({
-    //     marginLeft: '47%'
-    // }, 1750, 'swing', function() {
-    // });
-
-    // $("#subtitle").animate({
-    //     marginLeft: '-15%'
-    // }, 2250, 'swing', function() {
-    //     $("#content h2").fadeIn(1000);
-    //     $("#content p").fadeIn(1000);
-    // });
-
     // $("#title").css(
     //     'text-align', 'center');
 
@@ -59,50 +58,15 @@ $(document).ready(function() {
 
     function adjustStyle(width) {
       width = parseInt(width);
-      if (width < 651) {
-        // $("#size-stylesheet").attr("href", "css/mobile.css");
-
-        $("#title").stop().animate({
-            marginLeft: '0%',
-            borderBottom: '0px solid #fff',
-            textAlign: 'center'
-        }, 300, 'swing', function() {
-            // $("#content h2").fadeIn(1000);
-            // $("#content p").fadeIn(1000);
-        });
-
-        $("#subtitle").stop().animate({
-            marginLeft: '0%',
-            borderBottom: '0px solid #fff',
-            textAlign: 'center'
-        }, 300, 'swing', function() {
-            // $("#content h2").fadeIn(1000);
-            // $("#content p").fadeIn(1000);
-        });
-
-        // $("#logo").stop().animate({
-        //     width: '300px'
-        // }, 300, 'swing');
-
+      if (width < 701) {
+        $("#size-stylesheet").attr("href", "css/mobile.css");
+        $("#javascript").attr("src", "/mobile.js");
+      } else if (width < 900) {
+        $("#size-stylesheet").attr("href", "css/main.css");
+        $("#javascript").attr("src", "/main.js");
       } else {
-        //  $("#size-stylesheet").attr("href", "css/main.css");
-
-         $("#title").stop().animate({
-             marginLeft: '47%'
-         }, 1750, 'swing', function() {
-             // $("#content h2").fadeIn(1000);
-             // $("#content p").fadeIn(1000);
-         });
-
-         $("#subtitle").stop().animate({
-             marginLeft: '-15%'
-         }, 2250, 'swing', function() {
-         });
-
-        //  $("#logo").stop().animate({
-        //      width: '300px'
-        //  }, 300, 'swing');
-
+         $("#size-stylesheet").attr("href", "css/main.css");
+         $("#javascript").attr("src", "/main.js");
       }
     }
 
