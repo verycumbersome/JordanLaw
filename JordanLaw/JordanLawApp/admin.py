c239cb2afe1ff38b0e776e30789adb04c3b73bf5
verycumbersome <matthewsages@gmail.com>
2017-06-21 01:40:49 -0700

    Add basic blog post functionality

--- JordanLaw/JordanLawApp/admin.py
+++ JordanLaw/JordanLawApp/admin.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-from .models import Person
+from .models import Person, BlogPost
 
 admin.site.register(Person)
+admin.site.register(BlogPost)
--- JordanLaw/JordanLawApp/views.py
+++ JordanLaw/JordanLawApp/views.py
@@ -2,7 +2,7 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
-from .models import Person
+from .models import Person, BlogPost
 
 def index(request):
     people = Person.objects.all()
@@ -10,4 +10,9 @@ def index(request):
 
 def people(request, person):
     print person
-    return render(request, "people/", {})
+    return render(request, "people.html", {})
+
+def blog(request):
+
+    posts = BlogPost.objects.order_by('created_date').reverse()
+    return render(request, "blog.html", {'posts':posts})
