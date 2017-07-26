47888ffe85d14a84f3c87084a3e7a4ce29e9eebe
verycumbersome <matthewsages@gmail.com>
2017-07-25 23:03:10 -0700

    Fix merge conflicts

--- JordanLaw/JordanLawApp/models.py
+++ JordanLaw/JordanLawApp/models.py
@@ -2,16 +2,13 @@
 from __future__ import unicode_literals
 
 from django.db import models
-<<<<<<< HEAD
 from django.utils import timezone
-=======
->>>>>>> 0031f4fa9f51a8bb0c795b603550d28a5fc2e3bf
 
 class Person(models.Model):
     name = models.CharField(max_length=50)
     description = models.TextField(max_length=1000)
-    photo = models.ImageField(upload_to='peopleimages/', default='/JordanLawApp/static/images/imgnotfound.png')
-<<<<<<< HEAD
+    photo = models.ImageField(upload_to='peopleimages/',
+                              default='/JordanLawApp/static/images/imgnotfound.png')
 
     def __str__(self):
         return self.title
@@ -23,5 +20,3 @@ class BlogPost(models.Model):
 
     def __str__(self):
         return self.title
-=======
->>>>>>> 0031f4fa9f51a8bb0c795b603550d28a5fc2e3bf
