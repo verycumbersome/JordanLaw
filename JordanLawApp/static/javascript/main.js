2237788e9ed95dca3eefeb178bd40c7dd3ea2741
verycumbersome <matthewsages@gmail.com>
2017-09-04 19:30:09 -0700

    Cleanup

--- JordanLawApp/static/javascript/main.js
+++ JordanLawApp/static/javascript/main.js
@@ -4,44 +4,44 @@ $(document).ready(function() {
     var yOffset = '';
     var windowHeight;
     const menuSlideSpeed = 300;
-     
+
     $("html,body").animate({scrollTop: 0}, 100);
     $(".header").fadeOut(0);
-    
+
     $("#content h2").fadeOut(0);
-    
+
     // $("#missionstatement").fadeOut(0);
-    
-    $(".navbar").find(".active").removeClass("active");
-    
+
+    // $(".navbar").find(".active").removeClass("active");
+
     $("#aboutusdropdown").slideUp(0);
     $("#practicesdropdown").slideUp(0);
     $("#blogdropdown").slideUp(0);
     $("#contactdropdown").slideUp(0);
-    
-    $(".navbar a").on("click", function(){
-        $(".navbar").find(".active").removeClass("active");
-    });
-    
+
+    // $(".navbar a").on("click", function(){
+    //     $(".navbar").find(".active").removeClass("active");
+    // });
+
     if (yOffset > $("#autoscroll").position().top){
         alert("fuck");
     }
-    
+
     setInterval(function(){
         $("#scrolldownarrow").stop().effect("bounce", { times:3, easing:"easeInCubic" }, 'normal');
     }, 1500);
 
     $("#scrolldown").click(function(e) {
-        
+
         page.on("scroll mousedown wheel DOMMouseScroll mousewheel keyup touchmove", function(){
            page.stop();
         });
-    
+
         page.stop().animate({scrollTop: $("footer").position().top}, 1500, function(){
            page.off("scroll mousedown wheel DOMMouseScroll mousewheel keyup touchmove");
         });
-    
-       return false; 
+
+       return false;
     });
 });
 
@@ -50,16 +50,16 @@ $(window).scroll(function(event){
     yOffset = window.pageYOffset;
     windowHeight = window.innerHeight;
     var breakpoint = windowHeight/3;
-    
+
     if (yOffset > breakpoint){
-        $(".navbar").addClass('scrolldown');
+        // $(".navbar").addClass('scrolldown');
         $("#missionstatement").addClass('slideInUp');
         $("#missionstatement").addClass('scrolldown');
     }
     else{
-        $(".navbar").removeClass('scrolldown');
+        // $(".navbar").removeClass('scrolldown');
         // $("#missionstatement").removeClass('scrolldown');
-        
+
     }
-    
+
 });
