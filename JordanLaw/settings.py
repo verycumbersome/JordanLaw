b18b21768778ba9e26bd9e015f832d56a64ba89d
verycumbersome <matthewsages@gmail.com>
2017-09-09 14:23:00 -0700

    Fix project path in settings

--- JordanLaw/settings.py
+++ JordanLaw/settings.py
@@ -119,14 +119,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
-
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-)
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
---
+++ JordanLaw/static/javascript/main.js
@@ -0,0 +1,42 @@
+$(document).ready(function() {
+    var page = $("html, body");
+
+    $("html,body").animate({scrollTop: 0}, 100);
+
+    setInterval(function(){
+        $(".scrolldownarrow").stop().effect("bounce", { times:3, distance:20, easing:"easeInCubic" }, 'normal');
+    }, 1500);
+
+    $(".scrollbox").click(function(e) {
+
+        page.on("scroll mousedown wheel DOMMouseScroll mousewheel keyup touchmove", function(){
+           page.stop();
+        });
+
+        page.stop().animate({scrollTop: $("#about-me").position().top}, 1500, function(){
+           page.off("scroll mousedown wheel DOMMouseScroll mousewheel keyup touchmove");
+        });
+
+       return false;
+    });
+});
+
+$(window).scroll(function(event){
+
+    yOffset = window.pageYOffset;
+    windowHeight = window.innerHeight;
+    var breakpoint = windowHeight/3;
+
+    if (yOffset > breakpoint){
+        $(".navbar").addClass('scrolldown');
+        $(".navbar").addClass('navbar-inverse');
+        $(".navbar").removeClass('navbar-light');
+    }
+    else{
+        $(".navbar").removeClass('scrolldown');
+        $(".navbar").removeClass('navbar-inverse');
+        $(".navbar").addClass('navbar-light');
+
+    }
+
+});
