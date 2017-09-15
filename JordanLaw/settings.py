6c12da14e3f593ad9e947aef000cb7201430f5bf
verycumbersome <matthewsages@gmail.com>
2017-09-15 10:06:18 -0700

    Remove toolkit and works and practices from navbar

--- JordanLaw/settings.py
+++ JordanLaw/settings.py
@@ -25,7 +25,11 @@ SECRET_KEY = '75s=0tlj$6994t=ko9!*^#+g-0tn_j2875p(29xu16%36t(ipt'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'jordanlaw.herokuapp.com', 'jordanlawpc.com']
+ALLOWED_HOSTS = ['localhost',
+                 '127.0.0.1',
+                 'jordanlaw.herokuapp.com',
+                 'jordanlawpc.com',
+                 ]
 
 
 # Application definition
